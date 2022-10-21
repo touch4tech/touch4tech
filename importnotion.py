cb7876252cda7f7a530fa7142ec9ffa79222184c
oberron <one.annum@gmail.com>
2022-10-21 21:19:46 +0200

    Update importnotion.py
    clean up

--- importnotion.py
+++ importnotion.py
@@ -1,12 +1,7 @@
-from os import environ
-print("env var")
-i = 0
-for var in environ:
-  i+=1
-  print(f"var {i}",var,environ[var][:3])
-print("done notion2md v1")
-
 import subprocess
+from notion2md import readDatabase, process_page_results
+from notion2md import get_notion_headers, pageid_2_md
+
 gist = "28939ddb1fd4ea4721475cf2edf474e8"
 gist_url = f"https://gist.github.com/{gist}.git"
 output = subprocess.call(["git", "clone", gist_url])
@@ -15,20 +10,17 @@ if not output==0:
 output = subprocess.call(["mv",f"{gist}/notion2md.py","./notion2md.py"])
 if not output==0:
   print(f"mv error code: {output}")
-  
-from notion2md import readDatabase, process_page_results
-from notion2md import get_notion_headers, pageid_2_md
 
+# notion db id for frenchtouch.dev is:
 FT_dbid = 'af9dd2942ef74d7c9faf32e694055a7e'
+# get the GitHub secret which has the Notion Key
 my_secret = environ.get("NOTIONKEY")
-print(24,"my_secret1",my_secret[1:2])
 headers = get_notion_headers(my_secret)
 print("headers",headers)
 res = readDatabase(databaseId=FT_dbid, notion_headers=headers)
 res2 = process_page_results(res, headers)
 
 res_t = readDatabase(databaseId=res2["page_children_ids"][1], notion_headers=headers)
-print(res_t)
 md = pageid_2_md(res_t, headers)
 print(md)
 with open("test.md",'w') as fo:
