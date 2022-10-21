5776863b7cb8482c93adbae09b734e88fdbf0cd9
oberron <one.annum@gmail.com>
2022-10-21 13:54:30 +0200

    Update notion2md.py

--- notion2md.py
+++ notion2md.py
@@ -1,1 +1,11 @@
+from os import environ
+print("env var")
+i = 0
+for var in environ:
+  i+=1
+  if i>3:
+    break
+  print("var",var)
+  for k in environ[var]:
+    print("\t",k)
 print("done notion2md v1")
