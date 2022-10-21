6a39cf4dc128f073e1b7c2e3143d492a4a2be9ab
oberron <one.annum@gmail.com>
2022-10-21 13:57:05 +0200

    Update notion2md.py

--- notion2md.py
+++ notion2md.py
@@ -3,9 +3,7 @@ print("env var")
 i = 0
 for var in environ:
   i+=1
-  if i>3:
-    break
-  print("var",var)
+  print(f"var {i}",var)
   for k in environ[var]:
     print("\t",k)
 print("done notion2md v1")
