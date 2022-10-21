18152224ea802f31829ea24621ee861568a50195
oberron <one.annum@gmail.com>
2022-10-21 16:05:54 +0200

    Update notion2md.py

--- notion2md.py
+++ notion2md.py
@@ -4,6 +4,30 @@ i = 0
 for var in environ:
   i+=1
   print(f"var {i}",var)
-  for k in environ[var]:
-    print("\t",k)
 print("done notion2md v1")
+
+import subprocess
+gist = "28939ddb1fd4ea4721475cf2edf474e8"
+gist_url = f"https://gist.github.com/{gist}.git"
+output = subprocess.call(["git", "clone", gist_url])
+if not output==0:
+  print(f"gist error code: {output}")
+output = subprocess.call(["mv",f"{gist}/notion2md.py","./notion2md.py"])
+if not output==0:
+  print(f"mv error code: {output}")
+  
+from notion2md import readDatabase, process_page_results
+from notion2md import get_notion_headers, pageid_2_md
+
+FT_dbid = 'af9dd2942ef74d7c9faf32e694055a7e'
+my_secret = environ["NotionKey"]
+headers = get_notion_headers(my_secret)
+res = readDatabase(databaseId=FT_dbid, notion_headers=headers)
+res2 = process_page_results(res, headers)
+
+res_t = readDatabase(databaseId=res2["page_children_ids"][1], notion_headers=headers)
+print(res_t)
+md = pageid_2_md(res_t, headers)
+print(md)
+with open("test.md",'w') as fo:
+  fo.write(md)
